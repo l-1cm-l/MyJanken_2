0dfad9a9e868f55d4946751e2195cadd1bab151f
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-07-31 11:48:30 +0900

    [processing]

--- MyJanken_2/ContentView.swift
+++ MyJanken_2/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answerNumber = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            if answerNumber == 0{
+                Text("これからじゃんけんします")
+            } else if answerNumber == 1{
+                Image("gu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("グー")
+            } else if answerNumber == 2{
+                Image("choki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("チョキ")
+            } else{
+                Image("pa")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("パー")
+            }
+            
+            Button(action: {
+                var newAnswerNumber = 0
+                repeat{
+                    newAnswerNumber = Int.random(in:1...3)
+                }while answerNumber == newAnswerNumber
+                answerNumber = newAnswerNumber
+            }) {
+                Text("じゃんけんをする")
+            }
+        }
     }
 }
 
