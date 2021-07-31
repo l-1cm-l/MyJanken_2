5d554039793c9665855710440daae040c1d3b025
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-07-31 10:40:56 +0900

    Initial Commit

---
+++ MyJanken_2/MyJanken_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyJanken_2App.swift
+//  MyJanken_2
+//
+//  Created by administrator on 2021/07/31.
+//
+
+import SwiftUI
+
+@main
+struct MyJanken_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
